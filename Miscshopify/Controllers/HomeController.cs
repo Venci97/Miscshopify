8279d1771397326b395ddc6f3553498152fb3f97
Venci97 <saifiatipov@gmail.com>
2022-11-18 05:14:22 +0200

    Added Base Controller. Login, Register and Layout edited. Added Migration that came with Identity scaffolding.

--- Miscshopify/Controllers/HomeController.cs
+++ Miscshopify/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace Miscshopify.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
